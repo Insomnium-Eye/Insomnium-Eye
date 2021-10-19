96299f9e893e94816da299b1707ffef0b76f6d14
Insomnium-Eye <insomnium-eye@live.com>
2021-10-19 17:47:54 -0500

    added contact form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("http://localhost:3000/contact", {
+    let response = await fetch("https://insomnium-eye.com/#/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
@@ -22,23 +22,26 @@ const Contact = () => {
     let result = await response.json();
     alert(result.status);
   };
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" required />
-      </div>
-      <button type="submit">{status}</button>
-    </form>
-  );
+  <div className="layout">
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" required />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" required />
+        </div>
+        <button type="submit">{status}</button>
+      </form>
+    );
+  </div>
+  
 };
   
 export default Contact;
